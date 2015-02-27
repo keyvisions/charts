ae92f5701259e28e51f5e8daaf66c1cb1a38b678
Giancarlo Trevisan <g.trevisan@keyvisions.it>
2015-02-27 05:39:36 +0000

    Before grid

--- data/datasource.js
+++ data/datasource.js
@@ -1,35 +1,35 @@
 var pie = [{
     "label": "FIAT",
-    "value": 10
+    "value": [10]
 }, {
     "label": "ALFA",
-    "value": 20
+    "value": [20]
 }, {
     "label": "LANCIA",
-    "value": 40
+    "value": [40]
 }, {
     "label": "AUDI",
-    "value": 25
+    "value": [25]
 }];
 var line = [{
     "label": "Time (Hour)",
-    "value": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    "value": [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 },{
     "label": "Temperature (°C)",
-    "value": [17, 17, 18, 18.5, 18.6, 19, 19, 19.5, 20, 20, 21, 21]
+    "value": [17, 17, 18, 18.5, 18.6, 19, 19, 19.5, 20, 20, 21, 21, -5]
 },{
-    "label": "Temperature (°C)",
-    "value": [17, 15, 14, 13.5, 18.6, 19, 19, 19.5, 20, 20, 21, 21]
+    "label": "Pressure (atm)",
+    "value": [17, 15, 14, 13.5, 18.6, 19, 16, 19.5, 25, 20, 21, 21, -10, -23]
 }];
 var gauge = [{
     "label": "Democrats",
-    "value": 0.286
+    "value": [0.286]
 }, {
     "label": "Republicans",
-    "value": 0.5
+    "value": [0.5]
 }, {
     "label": "Liberals",
-    "value": 0.71
+    "value": [0.71]
 }];
 var gantt = [{
     "label": "stampa",
--- scripts/charts.js
+++ scripts/charts.js
@@ -13,37 +13,38 @@ window.chart = (function() {
     }
     function circle(r, cx, cy, attrs) {
         var obj = createSVGElement('circle', {
-            'r': r,
-            'cx': cx,
-            'cy': cy
+            'r': Math.round(r),
+            'cx': Math.round(cx),
+            'cy': Math.round(cy)
         });
         setAttributes(obj, attrs);
         return obj;
     }
     function rect(x, y, w, h, attrs) {
         var obj = createSVGElement('rect', {
-            'x': x,
-            'y': y,
-            'width': w,
-            'height': h
+            'x': Math.round(x),
+            'y': Math.round(y),
+            'width': Math.round(w),
+            'height': Math.round(h)
         });
         setAttributes(obj, attrs);
         return obj;
     }
-    function line(x1, y1, x2, y2, attrs) {
-            var obj = createSVGElement('line', {
-                'x1': x1,
-                'y1': y1,
-                'x2': x2,
-                'y2': y2
-            });
-            setAttributes(obj, attrs);
-            return obj;
-        }
     function text(x, y, txt, attrs) {
-        var obj = createSVGElement('text', { 'x': x, 'y': y, 'text-anchor': 'middle' });
+        var obj = createSVGElement('text', { 'x': Math.round(x), 'y': Math.round(y), 'text-anchor': 'middle' });
+        setAttributes(obj, attrs);
         obj.textContent = txt;
         return obj;
+    }
+
+    function grid(w, h, min, max) {
+        var d = '';
+        for (var i = Math.max(w, h); i >= 0 ; i -= 10)
+            d += 'M0 ' + i + ' H' + w + ' M' + i + ' 0 V' + h + ' ';
+        return createSVGElement('path', {
+            'class': 'grid',
+            'd': d
+        });
     }
     
     // alert(Math.log(w) / Math.log(10));
@@ -55,33 +56,30 @@ window.chart = (function() {
                 'height': h
             });
             // Grid
-            var d = '';
-            for (var i = 0; i <= Math.max(w, h); i += 10)
-                d += 'M0 ' + i + ' H' + w + ' M' + i + ' 0 V' + h + ' ';
-            svg.appendChild(createSVGElement('path', {
-                'class': 'grid',
-                'd': d
-            }));
-            // Axes
-            svg.appendChild(createSVGElement('path', {
-                'class': 'axes',
-                'd': 'M0,0 V' + h + ' M0,' + h + ' H' + w
-            }));
-            
+            if (!options || options['grid'] != false)
+                svg.appendChild(grid(w, h));
             // Points
             if (data.length > 0) {
-                var xo = Math.min.apply(Math, data[0].value), sx = w / (Math.max.apply(Math, data[0].value) - xo);
+                var xo = Math.min.apply(Math, data[0].value), sx = w / (Math.max.apply(Math, data[0].value) - xo), yo = Number.POSITIVE_INFINITY, sy;
                 for (var s = 1; s < data.length; ++s) {
-                    var yo = Math.min.apply(Math, data[s].value), sy = h / (Math.max.apply(Math, data[s].value) - yo);
-
-                    var points = '';
-                    for (var i = 0; i < data[0].value.length; ++i) {
-                        var x = Math.floor((data[0].value[i] - xo) * sx), y = h - Math.floor((data[s].value[i] - yo) * sy);
-                        svg.appendChild(circle(3, x, y, { 'style': 'stroke:none;fill:' + colors[s % 9] }));
+                    yo = Math.min(yo, Math.min.apply(Math, data[s].value)), sy = h / (Math.max.apply(Math, data[s].value) - yo);
+                }
+                sy *= 0.9;
+                // Axes
+                svg.appendChild(createSVGElement('path', {
+                    'class': 'axes',
+                    'd': 'M' + -Math.floor(xo * sx) + ',0 V' + h + ' M0,' + h + Math.floor(yo * sy) + ' H' + w
+                }));
+    
+                for (var s = 1; s < data.length; ++s) {
+                    var points = '', I = Math.min(data[0].value.length, data[s].value.length);
+                    for (var i = 0; i < I; ++i) {
+                        var x = (data[0].value[i] - xo) * sx, y = h - (data[s].value[i] - yo) * sy;
+                        svg.appendChild(circle(3, x, y, { 'style': 'stroke:none;fill:' + colors[(s-1) % 9] }));
                         points += x + ',' + y + ' ';
                     }
                     svg.appendChild(createSVGElement('polyline', {
-                        'style': 'stroke:' + colors[s % 9],
+                        'style': 'stroke:' + colors[(s-1) % 9],
                         'points': points
                     }));
                 }
@@ -94,25 +92,36 @@ window.chart = (function() {
                 'width': w,
                 'height': h
             });
-            var m = 1.1 * Math.max.apply(Math, data), d = '';
-            for (var i = 0; i <= w; i += 10) d += 'M0 ' + i + ' H' + w + ' ';
-            var element = createSVGElement('path', {
-                'class': 'grid',
-                'd': d
-            });
-            svg.appendChild(element);
-            var sets = 4,
-                dw = Math.floor((w - 5 * (sets + 2)) / data.length);
-            for (var i = 0; i < data.length; ++i) {
-                element = rect(i * dw + 5 * (Math.floor(i / sets) + 1), h - Math.floor(h * data[i] / m), dw - 1, Math.floor(h * data[i] / m));
-                element.setAttribute('style', 'fill:' + colors[i % sets]);
-                svg.appendChild(element);
+            var mh = 0, Mh = Number.NEGATIVE_INFINITY;
+            for (var s = 0; s < data.length; ++s) {
+                mh = Math.min(mh, Math.min.apply(Math, data[s].value)), Mh = Math.max(Mh, Math.max.apply(Math, data[s].value));
             }
-            svg.appendChild(line(0, 0, 0, h - 1, {
-                'class': 'axes'
-            }));
-            svg.appendChild(line(0, h - 1, w - 1, h - 1, {
-                'class': 'axes'
+            // Grid
+            var dh = 0.95 * h / (Mh - mh), d = '';
+            if (!options || options['grid'] != false) {
+                for (var i = w; i >= 0; i -= 10) 
+                    d += 'M0 ' + i + ' H' + w + ' ';
+                svg.appendChild(createSVGElement('path', {
+                    'class': 'grid',
+                    'd': d
+                }));
+            }
+            // Bars
+            var dw = (w - (data[0].value.length + 1)) / (data.length * data[0].value.length);
+            for (var i = 0; i < data[0].value.length; ++i) {
+                for (var s = 0; s < data.length; ++s) {
+                    var element;
+                    if (data[s].value[i] > 0)
+                        element = rect((s + data.length * i) * dw + i, h - (data[s].value[i] - mh) * dh, dw - 1, data[s].value[i] * dh, { 'style': 'fill:' + colors[s % 9] });
+                    else
+                        element = rect((s + data.length * i) * dw + i, h + (mh * dh), dw - 1, -data[s].value[i] * dh, { 'style': 'fill:' + colors[s % 9] });
+                    svg.appendChild(element);
+                }
+            }
+            // Axes
+            svg.appendChild(createSVGElement('path', {
+                'class': 'axes',
+                'd': 'M0,0 V' + h + ' M0,' + Math.floor(h + mh * dh) + ' H' + w
             }));
             return svg;
         },
@@ -122,20 +131,20 @@ window.chart = (function() {
                 'width': 2 * r,
                 'height': 2 * r
             });
-            var i, total = 0;
-            for (i = 0; i < data.length; ++i) 
-                total += data[i].value;
-            if (total > 0 && i > 1) {
+            var total = 0;
+            for (var s = 0; s < data.length; ++s) 
+                total += data[s].value[0];
+            if (total > 0 && s > 1) {
                 var startAngle, endAngle = 0;
-                for (i = 0; i < data.length; ++i) {
+                for (var s = 0; s < data.length; ++s) {
                     startAngle = endAngle;
-                    endAngle = startAngle + 2.0 * Math.PI * data[i].value / total;
+                    endAngle = startAngle + 2.0 * Math.PI * data[s].value[0] / total;
                     var slice = createSVGElement('path', {
-                        'style': 'fill:' + colors[i % 9],
-                        'd': 'M' + r + ',' + r + ' L' + Math.floor(r + r * Math.cos(startAngle)) + ',' + Math.floor(r + r * Math.sin(startAngle)) + ' A' + r + ',' + r + ' 0 ' + (2.0 * data[i].value > total ? 1 : 0) + ',1 ' + Math.floor(r + r * Math.cos(endAngle)) + ',' + Math.floor(r + r * Math.sin(endAngle))
+                        'style': 'fill:' + colors[s % 9],
+                        'd': 'M' + r + ',' + r + ' L' + Math.floor(r + r * Math.cos(startAngle)) + ',' + Math.floor(r + r * Math.sin(startAngle)) + ' A' + r + ',' + r + ' 0 ' + (2.0 * data[s].value[0] > total ? 1 : 0) + ',1 ' + Math.floor(r + r * Math.cos(endAngle)) + ',' + Math.floor(r + r * Math.sin(endAngle))
                     });
                     var title = createSVGElement('title');
-                    title.textContent = Math.floor(1000 * data[i].value / total) / 10.0 + '%';
+                    title.textContent = data[s].label + ' ' + Math.floor(1000 * data[s].value[0] / total) / 10.0 + '%';
                     slice.appendChild(title);
                     svg.appendChild(slice);
                 }
@@ -150,8 +159,33 @@ window.chart = (function() {
             svg.setAttribute('class', 'donut');
             return svg;
         },
+        // Requires x, y and r
         bubble: function(w, h, data, options) {
-            var svg = createSVGElement('svg');
+            var svg = createSVGElement('svg', {
+                'class': 'bubble',
+                'width': w,
+                'height': h
+            });
+            if (data.length === 3) {
+                var xo = Math.min.apply(Math, data[0].value), sx = w / (Math.max.apply(Math, data[0].value) - xo),
+                    yo = Math.min(yo, Math.min.apply(Math, data[1].value)), sy = h / (Math.max.apply(Math, data[1].value) - yo);
+
+                if (!options || options['grid'] != false)
+                    svg.appendChild(grid(w, h));
+    
+                sy *= 0.9;
+                // Axes
+                svg.appendChild(createSVGElement('path', {
+                    'class': 'axes',
+                    'd': 'M' + -Math.floor(xo * sx) + ',0 V' + h + ' M0,' + h + Math.floor(yo * sy) + ' H' + w
+                }));
+    
+                var I = data[0].value.length;
+                for (var i = 0; i < I; ++i) {
+                    var x = (data[0].value[i] - xo) * sx, y = h - (data[1].value[i] - yo) * sy;
+                    svg.appendChild(circle(data[2].value[i], x, y, { 'style': 'stroke:none;fill:' + colors[0] }));
+                }
+            }
             return svg;
         },
         map: function(w, h, data, options) {
@@ -159,41 +193,61 @@ window.chart = (function() {
             return svg;
         },
         radar: function(r, data, options) {
-            var s = 0;
             var svg = createSVGElement('svg', {
                 'class': 'radar',
                 'width': 2 * r,
                 'height': 2 * r
             });
-            var m = 1.1 * Math.max.apply(Math, data), a = 2.0 * Math.PI / data.length, d = '', points = '';
-            for (var i = 0; i < data.length; ++i) {
+            var m = Number.NEGATIVE_INFINITY;
+            for (var s = 0; s < data.length; ++s)
+                m = Math.max(m, Math.max.apply(Math, data[s].value));
+            m *= 1.1;
+            
+            // Axes
+            var a = 2 * Math.PI / data[0].value.length, d = '';
+            for (var i = 0; i < data[0].value.length; ++i)
                 d += 'M' + r + ',' + r + ' l' + Math.floor(r * Math.cos(a * i)) + ',' + Math.floor(r * Math.sin(a * i)) + ' ';
-                points += Math.floor(r + r * data[i] / m * Math.cos(a * i)) + ',' + Math.floor(r + r * data[i] / m * Math.sin(a * i)) + ' ';
-                svg.appendChild(circle(3, Math.floor(r + r * data[i] / m * Math.cos(a * i)), Math.floor(r + r * data[i] / m * Math.sin(a * i)), { 'style': 'stroke:none;fill:' + colors[s % 9] }));
-            }
-            svg.insertBefore(createSVGElement('path', {
+            svg.appendChild(createSVGElement('path', {
                 'd': d
-            }), svg.firstChild);
-            svg.appendChild(createSVGElement('polygon', {
-                'style': 'stroke:' + colors[s % 9],
-                'points': points 
             }));
-
+            // Points
+            for (s = 0; s < data.length; ++s) {
+                var points = '';
+                for (var i = 0; i < data[s].value.length; ++i) {
+                    var x = Math.floor(r + r * data[s].value[i] / m * Math.cos(a * i)), y = Math.floor(r + r * data[s].value[i] / m * Math.sin(a * i));
+                    points += x + ',' + y + ' ';
+                    svg.appendChild(circle(3, x, y, { 'style': 'stroke:none;fill:' + colors[s % 9] }));
+                }
+                svg.appendChild(createSVGElement('polygon', {
+                    'style': 'stroke:' + colors[s % 9],
+                    'points': points 
+                }));
+                svg.appendChild(text(x + 5, y + 5, data[s].label, {
+                    'style': 'stroke:' + colors[s % 9],
+                    'text-anchor': 'left'
+                }));
+            }
             return svg;
         },
         gauge: function(r, data, options) {
             var panel = document.createElement('span');
+            
+            var m = 0, M = 1.0;
+            if (options && options['min']) m = options['min'];
+            if (options && options['max']) M = options['max'];
+
             for (var i = 0; i < data.length; ++i) {
                 var svg = createSVGElement('svg', {
                         'class': 'gauge',
                         'width': 2 * r,
                         'height': 2 * r
                     }),
-                    sr = Math.floor(0.667 * r);
-                    
-                svg.appendChild(text(r, r, (Math.floor(1000 * data[i].value) / 10) + '%', { 'font-size': Math.floor(0.333 * r) + 'px' }));
+                    sr = Math.floor(0.667 * r),
+                    v = (data[i].value - m) / (M - m);
+                
+                svg.appendChild(text(r, r, (Math.floor(1000 * v) / 10) + '%', { 'font-size': Math.floor(0.333 * r) + 'px' }));
                 svg.appendChild(text(r, Math.floor(1.5 * r), data[i].label, { 'font-size': Math.floor(0.333 * r) + 'px' }));
-                var v = 1.0 - (data[i].value - Math.floor(data[i].value));
+                v = 1.0 - (v - Math.floor(v));
                 var element = createSVGElement('path', {
                     'd': 'M0,' + r + ' A' + r + ',' + r + ' 0 1,1 ' + Math.floor(r + r) + ',' + r + ' H' + Math.floor(r + sr) + ' A' + sr + ',' + sr + ' 0 0,0 ' + (r - sr) + ',' + r + ' z'
                 });
